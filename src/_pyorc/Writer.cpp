3f425c5390212838adc1a16338209119b0f0c334
noirello <noirello@gmail.com>
2019-10-02 20:55:35 +0200

    Add Writer with basic functionality

---
+++ src/_pyorc/Writer.cpp
@@ -0,0 +1,36 @@
+#include "Writer.h"
+#include "PyORCStream.h"
+
+Writer::Writer(py::object fileo, py::object schema_str, py::object batch_size) {
+    std::string schstr = py::cast<std::string>(schema_str);
+    std::unique_ptr<orc::Type> schema(orc::Type::buildTypeFromString(schstr));
+    orc::WriterOptions options;
+
+    currentRow = 0;
+    batchItem = 0;
+    outStream = std::unique_ptr<orc::OutputStream>(new PyORCOutputStream(fileo));
+    writer = createWriter(*schema, outStream.get(), options);
+    batchSize = py::cast<uint64_t>(batch_size);
+    batch = writer->createRowBatch(batchSize);
+    converter = createConverter(schema.get());
+}
+
+void Writer::write(py::object row) {
+    converter->write(batch.get(), batchItem, row);
+    currentRow++; batchItem++;
+
+    if (batchItem == batchSize) {
+        writer->add(*batch);
+        converter->clear();
+        batchItem = 0;
+    }
+}
+
+void Writer::close() {
+    if (batchItem != 0) {
+        writer->add(*batch);
+        converter->clear();
+        batchItem = 0;
+    }
+    writer->close();
+}
---
+++ src/_pyorc/Writer.h
@@ -0,0 +1,27 @@
+#ifndef WRITER_H
+#define WRITER_H
+
+#include <pybind11/pybind11.h>
+#include "orc/OrcFile.hh"
+
+#include "Converter.h"
+
+namespace py = pybind11;
+
+class Writer {
+private:
+    std::unique_ptr<orc::OutputStream> outStream;
+    std::unique_ptr<orc::Writer> writer;
+    std::unique_ptr<orc::ColumnVectorBatch> batch;
+    std::unique_ptr<Converter> converter;
+    uint64_t batchSize;
+    uint64_t batchItem;
+public:
+    uint64_t currentRow;
+
+    Writer(py::object, py::object, py::object = py::int_(1024));
+    void write(py::object);
+    void close();
+};
+
+#endif
