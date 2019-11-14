9b548fd733b6e2c9b9217e2788be8ca528cda0bf
noirello <noirello@gmail.com>
2019-11-14 22:50:48 +0100

    Update sphinx doc conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,20 +9,43 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import sys
+import os
+
+sys.path[0:0] = [os.path.abspath("..")]
+
+# For read-the-docs: mocking the _pyorc module.
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        if name == "typedescritpion":
+            return object
+        if name == "reader":
+            return object
+        if name == "writer":
+            return object
+
+
+MOCK_MODULES = ["src.pyorc._pyorc", "pyorc", "pyorc._pyorc"]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+import src.pyorc as pyorc
+
+sys.modules["pyorc"] = pyorc
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PyORC'
-copyright = '2019, noirello'
-author = 'noirello'
+project = "PyORC"
+copyright = "2019, noirello"
+author = "noirello"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,16 +53,15 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +69,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
