718c516ff10f65123a578faaf1fa43121ebc84ee
noirello <noirello@gmail.com>
2019-11-30 14:17:49 +0100

    Add ORCConverter abstarct class to helpers, implement Timestamp, Date and Decimal converters

--- src/pyorc/helpers.py
+++ src/pyorc/helpers.py
@@ -1,28 +1,86 @@
+from abc import ABC, abstractmethod
+
+from datetime import date, datetime, timezone
 from decimal import Decimal, localcontext
 
-def adjust_decimal(prec: int, scale: int, dec: Decimal) -> int:
-    """
-    Adjust the Decimal number to the given precision and scale.
-    Return the integer value of the number.
+from typing import Tuple
 
-    :param int prec: the precision of the decimal
-    :param int scale: the scale of decimal
-    :param decimal.Decimal dec: the number as Python decimal
-    :return: an integer (interpreted with the given scale)
-    :rtype: int
-    """
-    with localcontext() as ctx:
-        try:
-            ctx.prec = prec
-            coefficent = Decimal("1.{0}".format("0" * scale))
-            dec = dec.quantize(coefficent)
-            dec_tup = dec.as_tuple()
-            integer = sum(dig * 10 ** exp for exp, dig in enumerate(reversed(dec_tup.digits)))
-            if dec_tup.exponent > 0:
-                integer = integer * 10 ** dec_tup.exponent
-            if dec_tup.sign == 1:
-                return integer * -1
-            else:
-                return integer
-        except AttributeError:
-            raise TypeError("Item {0} cannot be cast as a decimal".format(type(dec))) from None
+from .enums import TypeKind
+
+
+class ORCConverter(ABC):
+    @staticmethod
+    @abstractmethod
+    def from_orc(*args):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def to_orc(*args):
+        pass
+
+
+class TimestampConverter(ORCConverter):
+    @staticmethod
+    def from_orc(seconds: int, nanoseconds: int) -> datetime:
+        timestamp = datetime.fromtimestamp(seconds, timezone.utc)
+        return timestamp.replace(microsecond=nanoseconds // 1000)
+
+    @staticmethod
+    def to_orc(obj: datetime) -> Tuple[int, int]:
+        return int(obj.replace(microsecond=0).timestamp()), obj.microsecond * 1000
+
+
+class DateConverter(ORCConverter):
+    @staticmethod
+    def from_orc(days: int) -> date:
+        return date.fromtimestamp(days * 24 * 60 * 60)
+
+    @staticmethod
+    def to_orc(obj: date) -> int:
+        return (obj - date(1970, 1, 1)).days
+
+
+class DecimalConverter(ORCConverter):
+    @staticmethod
+    def from_orc(decimal: str) -> Decimal:
+        return Decimal(decimal)
+
+    @staticmethod
+    def to_orc(precision: int, scale: int, obj: Decimal) -> int:
+        """
+        Adjust the Decimal number to the given precision and scale.
+        Return the integer value of the number.
+
+        :param int precision: the precision of the decimal
+        :param int scale: the scale of decimal
+        :param decimal.Decimal obj: the number as Python decimal
+        :return: an integer (interpreted with the given scale)
+        :rtype: int
+        """
+        with localcontext() as ctx:
+            try:
+                ctx.prec = precision
+                coefficient = Decimal("1.{0}".format("0" * scale))
+                dec = obj.quantize(coefficient)
+                dec_tup = dec.as_tuple()
+                integer = sum(
+                    dig * 10 ** exp for exp, dig in enumerate(reversed(dec_tup.digits))
+                )
+                if dec_tup.exponent > 0:
+                    integer = integer * 10 ** dec_tup.exponent
+                if dec_tup.sign == 1:
+                    return integer * -1
+                else:
+                    return integer
+            except AttributeError:
+                raise TypeError(
+                    "Item {0} cannot be cast as a decimal".format(type(dec))
+                ) from None
+
+
+DEFAULT_CONVERTERS = {
+    TypeKind.DATE: DateConverter,
+    TypeKind.DECIMAL: DecimalConverter,
+    TypeKind.TIMESTAMP: TimestampConverter,
+}
