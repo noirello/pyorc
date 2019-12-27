ad7eb245b994d14181bbe3e1226da1a7a1db9f23
noirello <noirello@gmail.com>
2019-12-28 00:36:20 +0100

    Add Column, remove C++ read_stripe bind

--- src/pyorc/reader.py
+++ src/pyorc/reader.py
@@ -1,8 +1,45 @@
+from collections import defaultdict
 from typing import Union, Optional, List, BinaryIO
 
-from pyorc._pyorc import reader
-from .enums import StructRepr
+from pyorc._pyorc import reader, stripe
+from .enums import StructRepr, TypeKind
 from .converters import DEFAULT_CONVERTERS
+
+
+class Column:
+    def __init__(self, stream, index):
+        self.index = index
+        self.stream = stream
+
+    @property
+    def statistics(self):
+        result = {}
+        result_list = defaultdict(list)
+        for stat in self.stream._statistics(self.index):
+            for key, val in stat.items():
+                result_list[key].append(val)
+        for key, values in result_list.items():
+            if key in (
+                "number_of_values",
+                "sum",
+                "false_count",
+                "true_count",
+                "total_length",
+            ):
+                result[key] = sum(values)
+            elif key in ("minimum", "lower_bound"):
+                result[key] = min(values)
+            elif key in ("maximum", "upper_bound"):
+                result[key] = max(values)
+            elif key == "has_null":
+                result[key] = any(values)
+        result["kind"] = TypeKind(result_list["kind"][0])
+        return result
+
+
+class Stripe(stripe):
+    def __getitem__(self, col_idx):
+        return Column(self, col_idx)
 
 
 class Reader(reader):
@@ -28,6 +65,12 @@ class Reader(reader):
         super().__init__(
             fileo, batch_size, column_indices, column_names, struct_repr, conv
         )
+
+    def __getitem__(self, col_idx):
+        return Column(self, col_idx)
+
+    def read_stripe(self, stripe_idx):
+        return Stripe(self, stripe_idx)
 
     def iter_stripes(self):
         for num in range(self.num_of_stripes):
