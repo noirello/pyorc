de1abca0c11957bd79f917a643fffa501b032de3
noirello <noirello@gmail.com>
2020-03-09 20:27:50 +0100

    Update tests for TypeDescription

--- tests/test_typedescription.py
+++ tests/test_typedescription.py
@@ -1,25 +1,21 @@
 import pytest
 
-from pyorc import typedescription
+from pyorc.typedescription import *
 from pyorc.enums import TypeKind
 
 
 def test_from_str_schema():
-    descr = typedescription(
+    descr = TypeDescription.from_string(
         "struct<a:int,b:map<varchar(20),int>,c:struct<d:bigint,e:float>>"
     )
     assert descr.kind == TypeKind.STRUCT
     assert len(descr.fields) == 3
     assert tuple(descr.fields.keys()) == ("a", "b", "c")
     assert descr.fields["a"].kind == TypeKind.INT
-    assert descr.fields["a"].precision is None
-    assert descr.fields["b"].scale is None
     assert descr.fields["b"].column_id == 2
-    assert len(descr.fields["b"].container_types) == 2
-    assert descr.fields["b"].container_types[0].kind == TypeKind.VARCHAR
-    assert descr.fields["b"].container_types[0].max_length == 20
-    assert descr.fields["b"].container_types[1].column_id == 4
-    assert descr.fields["c"].max_length is None
+    assert descr.fields["b"].key.kind == TypeKind.VARCHAR
+    assert descr.fields["b"].key.max_length == 20
+    assert descr.fields["b"].value.column_id == 4
     assert tuple(descr.fields["c"].fields.keys()) == ("d", "e")
     assert descr.fields["c"].fields["d"].kind == TypeKind.LONG
     assert descr.fields["c"].fields["e"].column_id == 7
@@ -27,129 +23,109 @@ def test_from_str_schema():
 
 def test_from_str_schema_fail():
     with pytest.raises(ValueError):
-        _ = typedescription(
+        _ = TypeDescription.from_string(
             "struct<a:int,b:map<varchar(20),in>,c:struct<d:bigint,e:float>>"
         )
     with pytest.raises(ValueError):
-        _ = typedescription("struct<a:int,")
+        _ = TypeDescription.from_string("struct<a:int,")
     with pytest.raises(TypeError):
-        _ = typedescription(2.5)
+        _ = TypeDescription.from_string(2.5)
 
 
 def test_find_column_id():
-    descr = typedescription("struct<a:struct<b:struct<c:int,d:string>,e:int>>")
+    descr = TypeDescription.from_string(
+        "struct<a:struct<b:struct<c:int,d:string>,e:int>>"
+    )
     assert descr.find_column_id("a") == 1
     assert descr.find_column_id("a.b.c") == 3
     assert descr.find_column_id("a.e") == 5
-    with pytest.raises(TypeError):
+    with pytest.raises(AttributeError):
         _ = descr.find_column_id(True)
     with pytest.raises(KeyError):
         _ = descr.find_column_id("f.z")
 
 
+TESTDATA = [
+    (Boolean(), TypeKind.BOOLEAN, "boolean"),
+    (TinyInt(), TypeKind.BYTE, "tinyint"),
+    (SmallInt(), TypeKind.SHORT, "smallint"),
+    (Int(), TypeKind.INT, "int"),
+    (BigInt(), TypeKind.LONG, "bigint"),
+    (Float(), TypeKind.FLOAT, "float"),
+    (Double(), TypeKind.DOUBLE, "double"),
+    (Date(), TypeKind.DATE, "date"),
+    (Timestamp(), TypeKind.TIMESTAMP, "timestamp"),
+    (String(), TypeKind.STRING, "string"),
+    (Binary(), TypeKind.BINARY, "binary"),
+    (Decimal(precision=10, scale=3), TypeKind.DECIMAL, "decimal(10,3)"),
+    (Char(16), TypeKind.CHAR, "char(16)"),
+    (VarChar(140), TypeKind.VARCHAR, "varchar(140)"),
+    (
+        Union(Int(), Double(), Char(20)),
+        TypeKind.UNION,
+        "uniontype<int,double,char(20)>",
+    ),
+    (Array(Int()), TypeKind.LIST, "array<int>"),
+    (Map(key=String(), value=Double()), TypeKind.MAP, "map<string,double>"),
+    (Struct(a=String(), b=Date()), TypeKind.STRUCT, "struct<a:string,b:date>"),
+    (
+        Struct(a=Timestamp(), b=Struct(c=Int(), b=Array(Double()))),
+        TypeKind.STRUCT,
+        "struct<a:timestamp,b:struct<c:int,b:array<double>>>",
+    ),
+]
+
+
+@pytest.mark.parametrize("orc_schema,kind,expected", TESTDATA)
+def test_str(orc_schema, kind, expected):
+    assert str(orc_schema) == expected
+
+
+@pytest.mark.parametrize("orc_schema,kind,expected", TESTDATA)
+def test_kind(orc_schema, kind, expected):
+    assert orc_schema.kind == kind
+
+
 def test_decimal():
-    descr = typedescription(TypeKind.DECIMAL)
-    assert descr.precision is None
-    assert descr.scale is None
-    descr.precision = 5
-    descr.scale = 3
+    descr = Decimal(precision=5, scale=3)
     assert descr.precision == 5
     assert descr.scale == 3
-    with pytest.raises(TypeError):
-        descr.precision = "f"
-    with pytest.raises(TypeError):
-        descr.scale = 3.14
     assert str(descr) == "decimal(5,3)"
 
 
-def test_decimal_fail():
-    descr = typedescription(TypeKind.VARCHAR)
-    with pytest.raises(TypeError):
-        descr.precision = 3
-    with pytest.raises(TypeError):
-        descr.scale = 2
-
-
 def test_varchar():
-    descr = typedescription("varchar(30)")
+    descr = TypeDescription.from_string("varchar(30)")
     assert descr.max_length == 30
     descr.max_length = 15
     assert descr.max_length == 15
-    with pytest.raises(TypeError):
-        descr.max_length = "f"
     assert str(descr) == "varchar(15)"
-    descr = typedescription(TypeKind.CHAR)
-    assert descr.max_length is None
+
+
+def test_char():
+    descr = Char(10)
+    assert descr.max_length == 10
     descr.max_length = 1
     assert str(descr) == "char(1)"
 
 
-def test_varchar_fail():
-    descr = typedescription(TypeKind.LONG)
+TESTDATA = [
+    (lambda: Struct(field0=Int(), field1=True),),
+    (lambda: Map(key=Int(), value=True),),
+    (lambda: Map(key=0, value=Double()()),),
+    (lambda: Array("test"),),
+    (lambda: Union(Int(), 0, Double()),),
+]
+
+
+@pytest.mark.parametrize("orc_schema", TESTDATA)
+def test_failed_complex_types(orc_schema):
     with pytest.raises(TypeError):
-        descr.max_length = 3
+        _ = orc_schema()
 
 
-def test_container_types():
-    descr = typedescription("map<string,int>")
-    assert len(descr.container_types) == 2
-    assert descr.container_types[0].kind == TypeKind.STRING
-    descr.container_types = (
-        typedescription(TypeKind.INT),
-        typedescription(TypeKind.STRING),
-    )
-    assert str(descr) == "map<int,string>"
-    assert descr.container_types[0].column_id == 1
-    with pytest.raises(ValueError):
-        descr.container_types = (typedescription(TypeKind.STRING),)
-    with pytest.raises(TypeError):
-        descr.container_types = (0, 1)
-    descr = typedescription("array<string>")
-    assert len(descr.container_types) == 1
-    descr.container_types = (typedescription(TypeKind.INT),)
-    assert str(descr) == "array<int>"
-    with pytest.raises(ValueError):
-        descr.container_types = (
-            typedescription(TypeKind.STRING),
-            typedescription(TypeKind.INT),
-        )
-    descr = typedescription("uniontype<string,int,double>")
-    assert len(descr.container_types) == 3
-    with pytest.raises(ValueError):
-        descr.container_types = []
-    descr = typedescription("string")
-    assert descr.container_types == []
-    with pytest.raises(TypeError):
-        descr.container_types = [typedescription(TypeKind.STRING)]
-
-
-def test_add_field():
-    descr = typedescription("string")
-    with pytest.raises(TypeError):
-        descr.add_field("test", typedescription(TypeKind.INT))
-    descr = typedescription(TypeKind.STRUCT)
-    assert descr.fields == {}
-    descr.add_field("a", typedescription(TypeKind.INT))
-    assert tuple(descr.fields.keys()) == ("a",)
-    assert descr.fields["a"].kind == TypeKind.INT
-    descr.add_field("b", typedescription(TypeKind.STRING))
-    assert tuple(descr.fields.keys()) == ("a", "b")
-    assert descr.fields["b"].column_id == 2
-    assert str(descr) == "struct<a:int,b:string>"
-    descr.add_field("a", typedescription(TypeKind.DOUBLE))
-    assert str(descr) == "struct<a:double,b:string>"
-
-
-def test_remove_field():
-    descr = typedescription("string")
-    with pytest.raises(TypeError):
-        descr.remove_field("test")
-    descr = typedescription("struct<a:double,b:string>")
-    assert descr.fields["b"].column_id == 2
-    assert "a" in descr.fields
-    descr.remove_field("a")
-    assert "a" not in descr.fields
-    with pytest.raises(KeyError):
-        descr.remove_field("c")
-    assert descr.fields["b"].column_id == 1
-    assert str(descr) == "struct<b:string>"
+def test_struct():
+    schema = Struct(a0=Int(), b0=Double(), c0=Struct(a1=Date(), b1=Timestamp()))
+    assert isinstance(schema["a0"], Int)
+    assert schema["b0"].kind == TypeKind.DOUBLE
+    assert schema["c0"].column_id == 3
+    assert schema["c0"]["b1"].kind == TypeKind.TIMESTAMP
