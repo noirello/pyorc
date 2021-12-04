b125de49a52140e563d2a1e41b2f2b35eeb405ed
noirello <noirello@gmail.com>
2021-12-04 01:00:57 +0100

    Extend test_writerows with empty iter test

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -407,6 +407,8 @@ def test_schema():
 def test_writerows():
     data = io.BytesIO()
     writer = Writer(data, "int")
+    res = writer.writerows([])
+    assert res == 0
     rows = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
     res = writer.writerows(rows)
     writer.close()
