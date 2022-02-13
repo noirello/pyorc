f09e07adfc0f2c7a49c4d9d8b9514fd618787d28
noirello <noirello@gmail.com>
2022-02-13 19:38:22 +0100

    Make ORC C++ Core library build part of build_ext
    Remove build_orc command

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import urllib.request
 import tarfile
 import logging
 
-from setuptools import setup, Command
+from setuptools import setup
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
@@ -16,29 +16,142 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
 
-class BuildORCLib(Command):
-    description = "run cmake to build ORC C++ Core library"
-    user_options = [
-        ("orc-version=", None, "the version of the ORC lib"),
+SOURCES = [
+    "_pyorc.cpp",
+    "Converter.cpp",
+    "PyORCStream.cpp",
+    "Reader.cpp",
+    "SearchArgument.cpp",
+    "Writer.cpp",
+]
+
+HEADERS = ["Converter.h", "PyORCStream.h", "Reader.h", "SearchArgument.h", "Writer.h"]
+
+if sys.platform.startswith("win32"):
+    LIBS = [
+        "orc",
+        "libprotobuf",
+        "libprotoc",
+        "lz4",
+        "zstd_static",
+        "zlibstatic",
+        "snappy",
+    ]
+else:
+    LIBS = ["orc", "protobuf", "protoc", "lz4", "zstd", "z", "snappy", "pthread"]
+
+EXT_MODULES = [
+    Pybind11Extension(
+        "pyorc._pyorc",
+        sources=[os.path.join("src", "_pyorc", src) for src in SOURCES],
+        depends=[os.path.join("src", "_pyorc", hdr) for hdr in HEADERS],
+        libraries=LIBS,
+        include_dirs=[os.path.join("deps", "include")],
+        library_dirs=[os.path.join("deps", "lib")],
+    )
+]
+
+
+class BuildExt(build_ext):
+    """
+    A custom build extension for build ORC Core library and handling
+    debug build on Windows.
+    """
+
+    user_options = build_ext.user_options + [
+        ("orc-version=", None, "the version of the ORC C++ Core library"),
         ("output-dir=", None, "the output directory"),
         ("source-url=", None, "the HTTP url for downloading the ORC source"),
-        ("build-type=", None, "set build type for ORC lib"),
-        ("download-only=", None, "just download and extract the ORC source"),
+        ("download-only", None, "just download and extract the ORC source"),
+        ("skip-orc-build", None, "skip bulding ORC C++ Core library"),
+    ]
+
+    boolean_options = build_ext.boolean_options + [
+        "download-only",
+        "skip-orc-build",
     ]
 
     def initialize_options(self):
         """Set default values for options."""
+        super().initialize_options()
         self.orc_version = "1.7.3"
         self.output_dir = "deps"
         self.source_url = "https://downloads.apache.org/orc/"
-        self.build_type = "debug"
         self.download_only = False
+        self.skip_orc_build = False
 
-    def finalize_options(self):
-        """Post-process options."""
-        pass
+    def _download_source(self) -> None:
+        tmp_tar = io.BytesIO()
+        url = "{url}orc-{ver}/orc-{ver}.tar.gz".format(
+            url=self.source_url, ver=self.orc_version
+        )
+        with urllib.request.urlopen(url) as src:
+            logging.info("Download ORC release from: %s" % url)
+            tmp_tar.write(src.read())
+        tmp_tar.seek(0)
+        tar_src = tarfile.open(fileobj=tmp_tar, mode="r:gz")
+        logging.info("Extract archives in: %s" % self.output_dir)
+        tar_src.extractall(self.output_dir)
+        tar_src.close()
 
-    def run(self):
+    @staticmethod
+    def _get_build_envs() -> dict:
+        env = os.environ.copy()
+
+        curr_cflags = env.get("CFLAGS", "")
+        curr_cxxflags = env.get("CXXFLAGS", "")
+        env["CFLAGS"] = f"{curr_cflags} -fPIC"
+        env["CXXFLAGS"] = f"{curr_cxxflags} -fPIC"
+
+        return env
+
+    def _build_with_cmake(self) -> str:
+
+        build_type = "DEBUG" if self.debug else "RELEASE"
+
+        cmake_args = [
+            f"-DCMAKE_BUILD_TYPE={build_type}",
+            "-DBUILD_JAVA=OFF",
+            "-DBUILD_LIBHDFSPP=OFF",
+            "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
+        ]
+        if sys.platform == "win32":
+            cmake_args.extend(
+                [
+                    "-DBUILD_CPP_TESTS=OFF",
+                    "-DBUILD_TOOLS=OFF",
+                    "-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded",
+                ]
+            )
+        env = self._get_build_envs()
+        build_dir = os.path.join(
+            self.output_dir, "orc-{ver}".format(ver=self.orc_version), "build"
+        )
+        if not os.path.exists(build_dir):
+            os.makedirs(build_dir)
+        logging.info("Build libraries with cmake")
+        cmake_cmd = ["cmake", ".."] + cmake_args
+        logging.info("Cmake command: %s" % cmake_cmd)
+        subprocess.check_call(cmake_cmd, cwd=build_dir, env=env)
+        if sys.platform == "win32":
+            subprocess.check_call(
+                [
+                    "cmake",
+                    "--build",
+                    ".",
+                    "--config",
+                    self.build_type,
+                    "--target",
+                    "PACKAGE",
+                ],
+                cwd=build_dir,
+                env=env,
+            )
+        else:
+            subprocess.check_call(["make", "-j4", "package"], cwd=build_dir, env=env)
+        return build_dir
+
+    def _build_orc_lib(self):
         if not os.path.isdir(
             os.path.join(self.output_dir, "orc-{ver}".format(ver=self.orc_version))
         ):
@@ -79,105 +192,16 @@ class BuildORCLib(Command):
             except Exception as exc:
                 logging.warning(exc)
 
-    def _download_source(self) -> None:
-        tmp_tar = io.BytesIO()
-        url = "{url}orc-{ver}/orc-{ver}.tar.gz".format(
-            url=self.source_url, ver=self.orc_version
-        )
-        with urllib.request.urlopen(url) as src:
-            logging.info("Download ORC release from: %s" % url)
-            tmp_tar.write(src.read())
-        tmp_tar.seek(0)
-        tar_src = tarfile.open(fileobj=tmp_tar, mode="r:gz")
-        logging.info("Extract archives in: %s" % self.output_dir)
-        tar_src.extractall(self.output_dir)
-        tar_src.close()
-
-    def _build_with_cmake(self) -> str:
-        cmake_args = [
-            "-DCMAKE_BUILD_TYPE={0}".format(self.build_type.upper()),
-            "-DBUILD_JAVA=OFF",
-            "-DBUILD_LIBHDFSPP=OFF",
-            "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
-        ]
-        if sys.platform == "win32":
-            cmake_args.extend(
-                [
-                    "-DBUILD_CPP_TESTS=OFF",
-                    "-DBUILD_TOOLS=OFF",
-                    "-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded",
-                ]
-            )
-        compiler_flags = ["CFLAGS=-fPIC", "CXXFLAGS=-fPIC"]
-        env = os.environ.copy()
-        build_dir = os.path.join(
-            self.output_dir, "orc-{ver}".format(ver=self.orc_version), "build"
-        )
-        if not os.path.exists(build_dir):
-            os.makedirs(build_dir)
-        logging.info("Build libraries with cmake")
-        env.update([flg.split("=") for flg in compiler_flags])
-        cmake_cmd = ["cmake", ".."] + cmake_args
-        logging.info("Cmake command: %s" % cmake_cmd)
-        subprocess.check_call(cmake_cmd, cwd=build_dir, env=env)
-        if sys.platform == "win32":
-            subprocess.check_call(
-                [
-                    "cmake",
-                    "--build",
-                    ".",
-                    "--config",
-                    self.build_type,
-                    "--target",
-                    "PACKAGE",
-                ],
-                cwd=build_dir,
-                env=env,
-            )
-        else:
-            subprocess.check_call(["make", "-j4", "package"], cwd=build_dir, env=env)
-        return build_dir
-
-
-SOURCES = [
-    "_pyorc.cpp",
-    "Converter.cpp",
-    "PyORCStream.cpp",
-    "Reader.cpp",
-    "SearchArgument.cpp",
-    "Writer.cpp",
-]
-HEADERS = ["Converter.h", "PyORCStream.h", "Reader.h", "SearchArgument.h", "Writer.h"]
-
-if sys.platform.startswith("win32"):
-    LIBS = [
-        "orc",
-        "libprotobuf",
-        "libprotoc",
-        "lz4",
-        "zstd_static",
-        "zlibstatic",
-        "snappy",
-    ]
-else:
-    LIBS = ["orc", "protobuf", "protoc", "lz4", "zstd", "z", "snappy", "pthread"]
-
-EXT_MODULES = [
-    Pybind11Extension(
-        "pyorc._pyorc",
-        sources=[os.path.join("src", "_pyorc", src) for src in SOURCES],
-        depends=[os.path.join("src", "_pyorc", hdr) for hdr in HEADERS],
-        libraries=LIBS,
-        include_dirs=[os.path.join("deps", "include")],
-        library_dirs=[os.path.join("deps", "lib")],
-    )
-]
-
-
-class BuildExt(build_ext):
-    """A custom build extension for handling debug build on Windows"""
-
     def build_extensions(self):
+        if not self.skip_orc_build:
+            orc_lib = os.path.join(
+                self.output_dir,
+                "lib",
+                "orc.lib" if sys.platform.startswith("win32") else "liborc.a",
+            )
+            if not os.path.exists(orc_lib):
+                self._build_orc_lib()
+
         if sys.platform.startswith("win32") and self.debug:
             self.extensions[0].libraries = [
                 lib if lib != "zlibstatic" else "zlibstaticd"
@@ -209,7 +233,7 @@ setup(
     packages=["pyorc"],
     package_data={"pyorc": ["py.typed", "_pyorc.pyi"]},
     include_package_data=True,
-    cmdclass={"build_ext": BuildExt, "build_orc": BuildORCLib},
+    cmdclass={"build_ext": BuildExt},
     keywords=["python3", "orc", "apache-orc"],
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -222,6 +246,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     python_requires=">=3.6",
     install_requires=[
