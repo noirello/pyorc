bd2ae86f028739c081ce273fed1feb6c8b2347b3
noirello <noirello@gmail.com>
2023-09-08 14:42:47 +0200

    Update doc requirements and year

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ sys.modules["pyorc"] = pyorc
 # -- Project information -----------------------------------------------------
 
 project = "PyORC"
-copyright = "2019-2022, noirello"
+copyright = "2019-2023, noirello"
 author = "noirello"
 
 # The full version, including alpha/beta/rc tags
